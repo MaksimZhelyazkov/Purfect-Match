e3435d565ba5a5211104f0e814e8538cc68e5493
Maxim <maximjekov@gmail.com>
2018-06-01 17:57:22 +0000

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v4");
+// mongoose.connect("mongodb://localhost/yelp_camp_v4");
+mongoose.connect("mongodb://maxim:maxim92@ds245170.mlab.com:45170/maxworkspace");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
