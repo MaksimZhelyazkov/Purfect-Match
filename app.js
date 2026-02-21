e4c9010c597eef64d5c44e84fa3e374e583fb405
Maksim Zhelyazkov <42282549+MaksimZhelyazkov@users.noreply.github.com>
2026-02-21 17:49:44 +0200

    Migrate app.js to use cats routes

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds")
-    
-// requiring routes
-var commentRoutes    = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index")
+var express        = require("express"),
+        app            = express(),
+        bodyParser     = require("body-parser"),
+        mongoose       = require("mongoose"),
+        flash          = require("connect-flash"),
+        passport       = require("passport"),
+        LocalStrategy  = require("passport-local"),
+        methodOverride = require("method-override"),
+        User           = require("./models/user");
 
-// mongoose.connect("mongodb://localhost/yelp_camp_v4");
-// mongoose.connect("mongodb://maxim:maxim92@ds245170.mlab.com:45170/maxworkspace");
+// Requiring routes
+var commentRoutes = require("./routes/comments"),
+        catRoutes     = require("./routes/cats"),
+        indexRoutes   = require("./routes/index");
+
 mongoose.connect(process.env.DATABASEURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
 });
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-// PASSPORT CONFIGURATION
 
+// Passport configuration
 app.use(require("express-session")({
-    secret: "dogs are the best",
-    resave: false,
-    saveUninitialized: false
+        secret: "cats are the best",
+        resave: false,
+        saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,17 +37,16 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+        res.locals.currentUser = req.user;
+        res.locals.error = req.flash("error");
+        res.locals.success = req.flash("success");
+        next();
 });
 
-
 app.use("/", indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/cats", catRoutes);
+app.use("/cats/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3000, function() {
-   console.log("The YelpCamp Server Has Started!");
+app.listen(process.env.PORT || 3000, function(){
+        console.log("Purfect Match Server Has Started!");
 });
