1f3349e7f6dd1b2ced6eeb5c1fb1eb0735b608c3
Maksim Zhelyazkov <42282549+MaksimZhelyazkov@users.noreply.github.com>
2026-02-21 17:22:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, function() {
    console.log("The YelpCamp Server Has Started!");
 });
