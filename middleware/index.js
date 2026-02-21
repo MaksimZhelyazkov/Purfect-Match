b13a0c4b086390da1a2fe78f4876d8011eaf2055
Maksim Zhelyazkov <42282549+MaksimZhelyazkov@users.noreply.github.com>
2026-02-21 17:56:11 +0200

    Migrate middleware to use Cat model and checkCatOwnership

--- middleware/index.js
+++ middleware/index.js
@@ -1,59 +1,56 @@
-var Campground = require("../models/campground");
+var Cat     = require("../models/cat");
 var Comment = require("../models/comment");
 
-// all the middleare goes here
 var middlewareObj = {};
 
-middlewareObj.checkCampgroundOwnership = function(req, res, next) {
- if(req.isAuthenticated()){
-        Campground.findById(req.params.id, function(err, foundCampground){
-           if(err || !foundCampground){
-               req.flash("error", "Campground not found");
-               res.redirect("back");
-           }  else {
-               // does user own the campground?
-            if(foundCampground.author.id.equals(req.user._id)) {
-                next();
-            } else {
-                req.flash("error", "You don't have permission to do that");
-                res.redirect("back");
-            }
-           }
-        });
-    } else {
-        req.flash("error", "You need to be logged in to do that");
-        res.redirect("back");
-    }
-}
+middlewareObj.checkCatOwnership = function(req, res, next) {
+     if(req.isAuthenticated()){
+              Cat.findById(req.params.id, function(err, foundCat){
+                           if(err || !foundCat){
+                                            req.flash("error", "Cat not found");
+                                            res.redirect("back");
+                           } else {
+                                            if(foundCat.author.id.equals(req.user._id)){
+                                                                 next();
+                                            } else {
+                                                                 req.flash("error", "You don't have permission to do that");
+                                                                 res.redirect("back");
+                                            }
+                           }
+              });
+     } else {
+              req.flash("error", "You need to be logged in to do that");
+              res.redirect("back");
+     }
+};
 
 middlewareObj.checkCommentOwnership = function(req, res, next) {
- if(req.isAuthenticated()){
-        Comment.findById(req.params.comment_id, function(err, foundComment){
-           if(err || !foundComment){
-               req.flash("error", "Comment not found");
-               res.redirect("back");
-           }  else {
-               // does user own the comment?
-            if(foundComment.author.id.equals(req.user._id)) {
-                next();
-            } else {
-                req.flash("error", "You don't have permission to do that");
-                res.redirect("back");
-            }
-           }
-        });
-    } else {
-        req.flash("error", "You need to be logged in to do that");
-        res.redirect("back");
-    }
-}
+     if(req.isAuthenticated()){
+              Comment.findById(req.params.comment_id, function(err, foundComment){
+                           if(err || !foundComment){
+                                            req.flash("error", "Comment not found");
+                                            res.redirect("back");
+                           } else {
+                                            if(foundComment.author.id.equals(req.user._id)){
+                                                                 next();
+                                            } else {
+                                                                 req.flash("error", "You don't have permission to do that");
+                                                                 res.redirect("back");
+                                            }
+                           }
+              });
+     } else {
+              req.flash("error", "You need to be logged in to do that");
+              res.redirect("back");
+     }
+};
 
 middlewareObj.isLoggedIn = function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "You need to be logged in to do that");
-    res.redirect("/login");
-}
+     if(req.isAuthenticated()){
+              return next();
+     }
+     req.flash("error", "You need to be logged in to do that");
+     res.redirect("/login");
+};
 
 module.exports = middlewareObj;
