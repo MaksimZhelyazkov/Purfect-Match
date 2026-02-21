fff3d9c36b264b8d21daac6e927e981386c21d39
Maksim Zhelyazkov <42282549+MaksimZhelyazkov@users.noreply.github.com>
2026-02-21 17:57:30 +0200

    Update seeds to use Cat model and cat data

--- seeds.js
+++ seeds.js
@@ -1,69 +1,68 @@
-var mongoose    = require("mongoose");
-var Campground  = require("./models/campground");
-var Comment     = require("./models/comment");
+var mongoose = require("mongoose");
+var Cat      = require("./models/cat");
+var Comment  = require("./models/comment");
 
 var data = [
     {
-        name: "Cloud's Rest", 
-        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "blah blah blah",
-        author:{
-            id : "588c2e092403d111454fff76",
-            username: "Jack"
-        }
+                name: "Whiskers",
+                age: 24,
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg",
+                description: "A playful tabby who loves cuddles and chasing toys.",
+                author: {
+                                id: "588c2e092403d111454fff76",
+                                username: "Admin"
+                }
     },
     {
-        name: "Desert Mesa", 
-        image: "http://www.bushtracks.com/assets/images/1280x475/southern-africa/botswana/xigera-camp/xigera-camp-okavango-delta-botswana-19-safari.jpg",
-        description: "blah blah blah",
-        author:{
-            id : "588c2e092403d111454fff76",
-            username: "Jack"
-        }
+                name: "Luna",
+                age: 12,
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Kittyply_edit1.jpg/1200px-Kittyply_edit1.jpg",
+                description: "A calm and gentle black cat who enjoys sunny windowsills.",
+                author: {
+                                id: "588c2e092403d111454fff76",
+                                username: "Admin"
+                }
     },
     {
-        name: "Canyon Floor", 
-        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "blah blah blah",
-        author:{
-            id : "588c2e092403d111454fff76",
-            username: "Jack"
-        }
+                name: "Oliver",
+                age: 36,
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
+                description: "An adventurous orange tabby looking for a loving home.",
+                author: {
+                                id: "588c2e092403d111454fff76",
+                                username: "Admin"
+                }
     }
-]
+    ];
 
 function seedDB(){
-    // REMOVE ALL CAMPGROUNDS
-    Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        }
-        console.log("removed campgrounds!")
-        // add a few campgrounds
-        data.forEach(function(seed){
-            Campground.create(seed, function(err, campground){
-                if(err){
-                    console.log(err)
-                } else {
-                    console.log("added a campground");
-                    // create a comment
-                    Comment.create(
-                        {
-                            text: "This is a great place, but not Wifi",
-                            author: "Homer"
-                        }, function (err, comment){
-                            if(err){
-                                console.log(err);
-                            } else {
-                                campground.comments.push(comment);
-                                campground.save(); 
-                                console.log("Created a new comment")
-                            }
+        Cat.deleteMany({}, function(err){
+                    if(err){
+                                    console.log(err);
+                    }
+                    console.log("Removed cats!");
+                    data.forEach(function(seed){
+                                    Cat.create(seed, function(err, cat){
+                                                        if(err){
+                                                                                console.log(err);
+                                                        } else {
+                                                                                console.log("Added a cat: " + cat.name);
+                                                                                Comment.create({
+                                                                                                            text: "Such a beautiful cat!",
+                                                                                                            author: { id: "588c2e092403d111454fff76", username: "Admin" }
+                                                                                    }, function(err, comment){
+                                                                                                            if(err){
+                                                                                                                                            console.log(err);
+                                                                                                                } else {
+                                                                                                                                            cat.comments.push(comment);
+                                                                                                                                            cat.save();
+                                                                                                                                            console.log("Created a comment");
+                                                                                                                }
+                                                                                    });
+                                                        }
+                                    });
                     });
-                }
-            });
         });
-    });
 }
 
 module.exports = seedDB;
